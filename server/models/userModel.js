ca7f4df5474ca4e7860543f3d14e120686cef8bc
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-01-26 17:17:54 +0530

    Updated user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,36 +1,41 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /.+\@.+\..+/
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
     },
     password: {
-        type: String,
-        required: true,
-        set: value => bcrypt.hashSync(value, 10)
+      type: String,
+      required: true,
+      set: (value) => bcrypt.hashSync(value, 10),
     },
-    streaks:{
-        type:Number,
-        default:0
+    streaks: {
+      type: Number,
+      default: 0,
     },
-    karmas:{
-        type:Number,
-        default:0
+    karmas: {
+      type: Number,
+      default: 0,
     },
     posts: [String],
     likedPosts: [String],
-    connections:[String],
-    badges:[String],
-    communities:[String]
-}, { timestamps: true });
+    connections: [String],
+    badges: [String],
+    communities: [String],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", userSchema)
-
+module.exports = mongoose.model("User", userSchema);
