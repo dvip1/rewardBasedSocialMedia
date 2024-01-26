0ccb1ce38e0311887d60fcffcd38f2ba8c5baad8
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-01-26 19:46:44 +0530

    Updated post controller

--- server/routes/postsRoute.js
+++ server/routes/postsRoute.js
@@ -1,16 +1,18 @@
 const postController = require("../controllers/postController");
 const authorize = require("../middleware/authorize");
 const router = require("express").Router();
+const media = require("../middleware/media");
 
-router.route("/post")
-    .post(authorize, postController.addPost)
-    .put(authorize, postController.updatePost)
+router
+  .route("/post")
+  .post(authorize, media.single("content-media"), postController.addPost)
+  .put(authorize, postController.updatePost);
 
-router.route("/post/:postId")
-    .get(authorize, postController.getPost)
-    .delete(authorize, postController.deletePost)
+router
+  .route("/post/:postId")
+  .get(authorize, postController.getPost)
+  .delete(authorize, postController.deletePost);
 
-router.route("/user/:userId")
-    .get(postController.getUserPost)
+router.route("/user/:userId").get(postController.getUserPost);
 
 module.exports = router;
