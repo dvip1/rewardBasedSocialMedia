4a67c4cea90dbe059af4d7bb65b90f52040d6054
Shaunak <shaunak.sastikar@gmail.com>
2024-01-27 02:02:27 +0530

    done with signin

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors")
 const logger = require("morgan");
 const connectDB = require("./configs/connect.js");
 const app = express();
