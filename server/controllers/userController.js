64b1a1517cecbeb293b9c1746d157c37ee2aa15c
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-01-27 02:36:06 +0530

    Created user controller to get user data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,46 +3,75 @@ const userModel = require("../models/userModel");
 
 const userController = {};
 
-userController.update = async (req, res) => {
-    const { name, email, userId } = req.body;
+userController.getUserProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await userModel.findById(userId);
 
-    if (! mongoose.isValidObjectId(userId)) return res.status(404).json({ message: "userId is not valid" })
-
-    if (!name.trim() || !email.trim())
-        return res.status(400).json({ message: "all fields required" })
-    try {
-        const updated = await userModel.findByIdAndUpdate(
-            { _id: userId },
-            { name: name, email: email },
-            { new: true }).select("name email");
-
-        if (updated) res.json(updated)
-        else res.json({ message: "failed to update, user not found" })
-    } catch (e) {
-        return res.status(500).json(e)
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+userController.update = async (req, res) => {
+  const { name, email, userId } = req.body;
+
+  if (!mongoose.isValidObjectId(userId))
+    return res.status(404).json({ message: "userId is not valid" });
+
+  if (!name.trim() || !email.trim())
+    return res.status(400).json({ message: "all fields required" });
+  try {
+    const updated = await userModel
+      .findByIdAndUpdate(
+        { _id: userId },
+        { name: name, email: email },
+        { new: true }
+      )
+      .select("name email");
+
+    if (updated) res.json(updated);
+    else res.json({ message: "failed to update, user not found" });
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+};
 
 userController.getUser = async (req, res) => {
-    const userId = req.params.userId;
-    if (! mongoose.isValidObjectId(userId)) return res.status(404).json({ message: "userId is not valid" })
-    const user = await userModel.findOne({ _id: userId }).select("-password").lean().exec();
-    if (user) res.json(user)
-    else res.json({ message: "user not found" })
-}
+  const userId = req.params.userId;
+  if (!mongoose.isValidObjectId(userId))
+    return res.status(404).json({ message: "userId is not valid" });
+  const user = await userModel
+    .findOne({ _id: userId })
+    .select("-password")
+    .lean()
+    .exec();
+  if (user) res.json(user);
+  else res.json({ message: "user not found" });
+};
 
 userController.getAllUser = async (req, res) => {
-    const user = await userModel.find({}).select("name email").lean().exec();
-    res.json(user)
-}
+  const user = await userModel.find({}).select("name email").lean().exec();
+  res.json(user);
+};
 
-userController.getAllConnections = async (req, res)=>{
-    const userId = req.user.id;
-    console.log(userId);
-    if (! mongoose.isValidObjectId(userId)) return res.status(404).json({ message: "userId is not valid" });
+userController.getAllConnections = async (req, res) => {
+  const userId = req.user.id;
+  console.log(userId);
+  if (!mongoose.isValidObjectId(userId))
+    return res.status(404).json({ message: "userId is not valid" });
 
-    const connections = await userModel.findById({_id:userId}).select("connections").lean().exec();
-    res.json(connections)
-}
+  const connections = await userModel
+    .findById({ _id: userId })
+    .select("connections")
+    .lean()
+    .exec();
+  res.json(connections);
+};
 
 module.exports = userController;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,16 +2,13 @@ const userController = require("../controllers/userController");
 const authorize = require("../middleware/authorize");
 const router = require("express").Router();
 
-router.route("/get")
-    .get(userController.getAllUser)
+router.route("/get").get(userController.getAllUser);
 
-router.route("/get/:userId")
-    .get(userController.getUser)
+router.route("/get/:userId").get(userController.getUser);
 
-router.route("/update")
-    .put(userController.update)
+router.route("/update").put(userController.update);
 
-router.route("/connctions")
-    .get(authorize, userController.getAllConnections)
+router.route("/myProfile").get(authorize, userController.getUserProfile);
+router.route("/connctions").get(authorize, userController.getAllConnections);
 
 module.exports = router;
