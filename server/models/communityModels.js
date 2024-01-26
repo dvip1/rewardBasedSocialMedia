faee6e80e8138b77110bdb413f393e428ffd5ca9
dvip1 <dvipatwork@outlook.com>
2024-01-26 19:40:36 +0530

    add leader in communityModels

---
+++ server/models/communityModels.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+
+const communitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Community name cannot be empty'],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Description cannot be empty'],
+        trim: true
+    },
+    leader: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+    ,
+    tasks: [{
+        description: {
+        type: String,
+    },
+    deadline: {
+        type: Date
+    },
+}]
+}, { timestamps: true })
+
+communitySchema.index({ members: 1 });
+
+module.exports = mongoose.model("Community", communitySchema);
