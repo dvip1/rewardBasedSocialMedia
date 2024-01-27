cc0d9556ba9fa1841a041d5098a4874ba86d5666
ankit <code7lancer@gmail.com>
2024-01-27 12:58:21 -0800

    created createTask controller

--- server/routes/communityRoutes.js
+++ server/routes/communityRoutes.js
@@ -7,8 +7,12 @@ router.route("/community")
   .post(authorize, communityController.createCommunity)
   .get(communityController.getAllCommunity)
   .put(authorize, communityController.addMember)
+ 
+router.route('/community/createTask')
+    .post(authorize, communityController.createTask)
 
 router.route("/community/:communityId")
   .get(communityController.getCommunity)
+
 
 module.exports = router;
